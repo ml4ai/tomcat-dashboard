777cab4226d0f26d6919d4458f44cf5b342eac56
Vincent Raymond <vraymond6@arizona.edu>
2022-05-18 08:52:23 -0400

    Loads frames from XRC instead of C++

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,4 +1,3 @@
-// wxWidgets "Hello World" Program
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
 #include <wx/xrc/xmlres.h>
@@ -10,10 +9,10 @@
 #include <string>
 #include <vector>
 
-#include "Frontend.h"
+//#include "Frontend.h"
 #include "Widget.h"
 #include "ASRWidget.h"
-#include "ScoreWidget.h"
+//#include "ScoreWidget.h"
 
 using namespace std;
 
@@ -24,30 +23,43 @@ public:
   void Update();
  
   vector<Widget *>widgets; 
-  UtteranceFrame *utterance_frame;
-  ScoreFrame *score_frame;
+  wxFrame *utterance_frame;
+  wxFrame *score_frame;
 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
+  // Load XRC files
+  wxXmlResource::Get()->InitAllHandlers();
+  wxXmlResource::Get()->LoadAllFiles("templates");
   
-  
-  // Generate ASRWidgets 
-  this->utterance_frame = new UtteranceFrame(NULL);
-  this->widgets.push_back(new ASRWidget(this->utterance_frame));
-  this->widgets.push_back(new ASRWidget(this->utterance_frame));
-  this->widgets.push_back(new ASRWidget(this->utterance_frame));
+
+  // Generate ASRWidgets
+  utterance_frame = new wxFrame(); 
+  if(wxXmlResource::Get()->LoadFrame(utterance_frame, NULL, "Utterance Frame")){
+	  widgets.push_back(new ASRWidget(utterance_frame));
+	  widgets.push_back(new ASRWidget(utterance_frame));
+	  widgets.push_back(new ASRWidget(utterance_frame));	  
+	  
+	  utterance_frame->Show();
+	  utterance_frame->Maximize();  
+	  utterance_frame->Refresh();
+  }
+  else{
+  	std::cout << "Failed to load Utterance frame" << std::endl; 
+  }
   
   // Generate Score Widget
-  this->score_frame = new ScoreFrame(NULL);
+  /*this->score_frame = new ScoreFrame(NULL);
   this->widgets.push_back(new ScoreWidget(this->score_frame)); 
   
   // Show frame
   this->score_frame->Show();
   this->score_frame->Maximize();  
-  this->score_frame->Refresh();
+  this->score_frame->Refresh();*/
   
+
   Connect( wxID_ANY, wxEVT_IDLE, wxIdleEventHandler(MyApp::onIdle) ); 
   return true;
 }
