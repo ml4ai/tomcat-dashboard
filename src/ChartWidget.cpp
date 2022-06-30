33d117520ef488563da0fdc1644bfc4bb115d3e4
Vincent Raymond <vraymond6@arizona.edu>
2022-06-30 11:10:25 -0400

    Switching references from ScoreWidget to ChartWidget

---
+++ src/ChartWidget.cpp
@@ -0,0 +1,113 @@
+
+#include <wx/wxprec.h>
+#include <wx/xrc/xmlres.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include <mathplot.h>
+#include <wx/filename.h>
+#include <wx/image.h>
+#include <wx/intl.h>
+#include <wx/listctrl.h>
+#include <wx/log.h>
+#include <wx/print.h>
+#include <wx/sizer.h>
+
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <streambuf>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <boost/log/trivial.hpp>
+#include <nlohmann/json.hpp>
+
+#include "ChartWidget.h"
+
+using namespace std;
+
+ChartWidget::ChartWidget(wxPanel *panel, string type, string mqtt_host, string mqtt_port) : Widget(type, mqtt_host, mqtt_port) {
+  // Set Frame
+  this->panel = panel;
+
+  // Get data from configuration
+  panel_name = configuration["panel_name"];
+  x_axis_field = configuration["x_axis_field"];
+  x_axis_label = configuration["x_axis_label"];
+  y_axis_field = configuration["y_axis_field"];
+  y_axis_label = configuration["y_axis_label"];
+
+  // Create and attach MathPlot chart to frame
+  chart = new mpWindow(panel, -1, wxPoint(0, 0), wxSize(500, 500),
+                             wxSUNKEN_BORDER);
+  chart->AddLayer(new mpScaleX(x_axis_label, mpALIGN_BOTTOM, true));
+  chart->AddLayer(new mpScaleY(y_axis_label, mpALIGN_LEFT, true));
+
+  data_vector = new mpFXYVector();
+  x_axis_data.insert(x_axis_data.end(), {});
+  y_axis_data.insert(y_axis_data.end(), {});
+  
+  data_vector->SetData(x_axis_data, y_axis_data);
+
+  wxPen vectorpen(*wxBLUE, 5, wxSOLID);
+  data_vector->SetPen(vectorpen);
+  chart->AddLayer(data_vector);
+
+  chart->Fit();
+}
+
+
+void ChartWidget::OnMessage(std::string topic,
+                            std::string message) {
+  nlohmann::json update;
+
+  // Parse JSON response
+  nlohmann::json response;
+  try {
+    response = nlohmann::json::parse(message);
+  } catch (exception e) {
+    std::cout << "Failed to parse Chart message" << std::endl;
+    return;
+  }
+
+  // Check for existence of x_axis_field in message
+  if (response["data"].contains(x_axis_field)) {
+    update["x_point"] = response["data"][x_axis_field];
+  } 
+  else{
+	BOOST_LOG_TRIVIAL(error) << "Missing fields in update message";
+	return;
+  }
+
+  
+  // Check for existence of y_axis_field in message
+  if (response["data"].contains(y_axis_field)) {
+    update["y_point"] = response["data"][y_axis_field];
+  } 
+  else{
+	BOOST_LOG_TRIVIAL(error) << "Missing fields in update message"; 	
+	return;
+  }
+
+  PushUpdate(update);
+}
+
+void ChartWidget::Update() {
+  // Check for update
+  if (UpdateQueued()) {
+    	nlohmann::json update = GetUpdate();
+	std::cout << update["x_point"] << std::endl;
+	std::cout << update["y_point"] << std::endl;
+	x_axis_data.push_back(update["x_point"]);
+	y_axis_data.push_back(update["y_point"]);
+	data_vector->SetData(x_axis_data, y_axis_data);
+
+	chart->Fit();
+	chart->Refresh(); // mpWindow must be refreshed after setting data
+  }
+}
+
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,3 +1,8 @@
+#include "Widget.h"
+#include "ASRWidget.h"
+#include "ChartWidget.h"
+#include "TrialWidget.h"
+
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
 #include <wx/xrc/xmlres.h>
@@ -7,15 +12,17 @@
 #include <wx/richtext/richtextctrl.h>
 #endif
 
+#include <nlohmann/json.hpp>
+
+#include <boost/log/trivial.hpp>
+
 #include <memory>
 #include <string>
 #include <vector>
 #include <map>
-
-#include "ASRWidget.h"
-#include "ScoreWidget.h"
-#include "TrialWidget.h"
-#include "Widget.h"
+#include <fstream>
+#include <iostream>
+#include <streambuf>
 
 using namespace std;
 
@@ -27,9 +34,11 @@ public:
   // Menu button press listeners
   void onTrialPress(wxCommandEvent &event);
   void onASRPress(wxCommandEvent &event);
-  void onScorePress(wxCommandEvent &event);
+  void onChartPress(wxCommandEvent &event);
   
   void Update();
+
+  void LoadAppConfig();
 
   vector<Widget *> widgets;
   
@@ -39,14 +48,18 @@ public:
   wxPanel *current_panel; // Need access to current panel to hide on switch
   wxPanel *utterance_panel;
   wxPanel *trial_panel;
-  wxPanel *score_panel;
+  wxPanel *chart_panel;
+
+  nlohmann::json app_configuration;
+  string mqtt_host;
+  string mqtt_port;
 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-  std::string mqtt_host = "localhost";
-  std::string mqtt_port = "1883";
+  // Load app configuration
+  LoadAppConfig();
 
   // Load XRC files
   wxXmlResource::Get()->AddHandler(new wxRichTextCtrlXmlHandler);
@@ -60,27 +73,27 @@ bool MyApp::OnInit() {
   // Load wxPanels
   trial_panel = (wxPanel *)frame->FindWindowByName("TRIAL_PANEL");
   utterance_panel = (wxPanel *)frame->FindWindowByName("UTTERANCE_PANEL");
-  score_panel = (wxPanel *)frame->FindWindowByName("SCORE_PANEL");
-  current_panel = score_panel; // Set default panel
+  chart_panel = (wxPanel *)frame->FindWindowByName("CHART_PANEL");
+  current_panel = utterance_panel; // Set default panel
   panel_sizer = current_panel->GetContainingSizer(); // Can't access sizers with FindWindowByName
 
   
   // Bind button press event listeners
   const char *trial_button_name = "TRIAL_BUTTON";
   const char *asr_button_name = "ASR_BUTTON";
-  const char *score_button_name = "CHART_BUTTON";
+  const char *chart_button_name = "CHART_BUTTON";
   int trial_button_id = XRCID(trial_button_name);
   int asr_button_id = XRCID(asr_button_name);
-  int score_button_id = XRCID(score_button_name);
+  int chart_button_id = XRCID(chart_button_name);
 
   frame->Bind(wxEVT_BUTTON, &MyApp::onTrialPress, this, trial_button_id);
   frame->Bind(wxEVT_BUTTON, &MyApp::onASRPress, this, asr_button_id);
-  frame->Bind(wxEVT_BUTTON, &MyApp::onScorePress, this, score_button_id);
+  frame->Bind(wxEVT_BUTTON, &MyApp::onChartPress, this, chart_button_id);
   
   widgets.push_back(
       new ASRWidget(utterance_panel, "ASRWidget", mqtt_host, mqtt_port));
   widgets.push_back(
-      new ScoreWidget(score_panel, "ScoreWidget", mqtt_host, mqtt_port));
+      new ChartWidget(chart_panel, "ChartWidget", mqtt_host, mqtt_port));
   widgets.push_back(
       new TrialWidget(trial_panel,"TrialWidget", mqtt_host, mqtt_port));
 
@@ -102,6 +115,21 @@ void MyApp::Update() {
   }
 }
 
+void MyApp::LoadAppConfig() {
+  fstream stream("config/app.json");
+  string hold((istreambuf_iterator<char>(stream)), istreambuf_iterator<char>());
+  try {
+    app_configuration = nlohmann::json::parse(hold);
+
+  } catch (exception e) {
+    BOOST_LOG_TRIVIAL(error)
+        << "Unable to load app.json configuration file";
+    exit(0);
+  }
+  mqtt_host = app_configuration["mqtt_host"];
+  mqtt_port = app_configuration["mqtt_port"];
+}
+
 void MyApp::onIdle(wxIdleEvent &evt) {
   this->Update();
   evt.RequestMore(); // render continuously, not only once on idle
@@ -119,9 +147,9 @@ void MyApp::onASRPress(wxCommandEvent &event){
 	utterance_panel->Show();
 	panel_sizer->Layout();
 }
-void MyApp::onScorePress(wxCommandEvent &event){
+void MyApp::onChartPress(wxCommandEvent &event){
 	current_panel->Hide();
-	current_panel = score_panel;
-	score_panel->Show();
+	current_panel = chart_panel;
+	chart_panel->Show();
 	panel_sizer->Layout();
 }
